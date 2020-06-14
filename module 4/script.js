869621b2d7681a14e1aee498f8ea4fe51efc59ce
Harshel Dola <65818703+Harshel88@users.noreply.github.com>
2020-06-14 11:55:56 +0530

    Update script.js

--- module 4/script.js
+++ module 4/script.js
@@ -1,12 +1,14 @@
-(function (window) {
+function (window) {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i in names) {
   var firstLetter = ((names[i]).charAt(0)).toLowerCase();
   if (firstLetter == 'j') {
      window.byeSpeaker.speak(names[i]);
-  } else {
+  } 
+  else {
      window.helloSpeaker.speak(names[i]);
   }
 }
-})(window);
-© 2020 GitHub, Inc.
+}
+window();
+
