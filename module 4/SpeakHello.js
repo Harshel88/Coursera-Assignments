bdf7ae2cdaa9a747930ab220b5a25f65a389beae
Harshel Dola <65818703+Harshel88@users.noreply.github.com>
2020-06-15 19:40:07 +0530

    Update SpeakHello.js

--- module 4/SpeakHello.js
+++ module 4/SpeakHello.js
@@ -1,4 +1,4 @@
-function (window) {
+(function (window) {
 var helloSpeaker = {};
 var speakWord = "Hello";
 helloSpeaker.speak = function (name) {
@@ -6,5 +6,4 @@ helloSpeaker.speak = function (name) {
 }
 window.helloSpeaker = helloSpeaker;
 
-}
-window();
+})(window);
